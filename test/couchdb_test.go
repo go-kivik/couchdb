e28d44d7509f47b3d75c2d24db840410c992fc8a
Jonathan Hall <flimzy@flimzy.com>
2017-10-23 11:45:39 +0200

    Copy Couch 2.0 test suite to 2.1

--- test/couchdb_test.go
+++ test/couchdb_test.go
@@ -17,10 +17,14 @@ func TestCouch16(t *testing.T) {
 	kiviktest.DoTest(kiviktest.SuiteCouch16, "KIVIK_TEST_DSN_COUCH16", t)
 }
 
-func TestCloudant(t *testing.T) {
-	kiviktest.DoTest(kiviktest.SuiteCloudant, "KIVIK_TEST_DSN_CLOUDANT", t)
-}
-
 func TestCouch20(t *testing.T) {
 	kiviktest.DoTest(kiviktest.SuiteCouch20, "KIVIK_TEST_DSN_COUCH20", t)
+}
+
+func TestCouch21(t *testing.T) {
+	kiviktest.DoTest(kiviktest.SuiteCouch21, "KIVIK_TEST_DSN_COUCH21", t)
+}
+
+func TestCloudant(t *testing.T) {
+	kiviktest.DoTest(kiviktest.SuiteCloudant, "KIVIK_TEST_DSN_CLOUDANT", t)
 }
