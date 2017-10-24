1512d1c58e6ddf3d391ae1f783003b30103f5f00
Jonathan Hall <flimzy@flimzy.com>
2017-10-24 23:29:44 +0200

    Complete test coverage for rowsQuery() function

--- db_test.go
+++ db_test.go
@@ -2,37 +2,24 @@ package couchdb
 
 import (
 	"context"
+	"errors"
 	"io"
+	"io/ioutil"
+	"net/http"
 	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/flimzy/diff"
 	"github.com/flimzy/kivik"
 	"github.com/flimzy/kivik/driver"
+	"github.com/flimzy/testy"
 )
 
 func TestAllDocs(t *testing.T) {
-	client := getClient(t)
-	db, err := client.DB(context.Background(), "_users", kivik.Options{"force_commit": true})
-	if err != nil {
-		t.Fatalf("Failed to connect to db: %s", err)
-	}
-	rows, err := db.AllDocs(context.Background(), map[string]interface{}{
-		"include_docs": true,
-	})
-	if err != nil {
-		t.Fatalf("Failed: %s", err)
-	}
-
-	for {
-		err := rows.Next(&driver.Row{})
-		if err != nil {
-			if err != io.EOF {
-				t.Fatalf("Iteration failed: %s", err)
-			}
-			break
-		}
-	}
+	db := newTestDB(nil, errors.New("test error"))
+	_, err := db.AllDocs(context.Background(), nil)
+	testy.Error(t, "Get http://example.com/testdb/_all_docs: test error", err)
 }
 
 func TestDBInfo(t *testing.T) {
@@ -194,6 +181,181 @@ func TestDBDelete(t *testing.T) {
 			}
 			if errMsg != test.err || status != test.status {
 				t.Errorf("Unexpected error: %d / %s", status, errMsg)
+			}
+		})
+	}
+}
+
+func TestRowsQuery(t *testing.T) {
+	type queryResult struct {
+		Offset    int64
+		TotalRows int64
+		Warning   string
+		UpdateSeq string
+		Err       string
+		Rows      []driver.Row
+	}
+	tests := []struct {
+		name     string
+		db       *db
+		path     string
+		options  map[string]interface{}
+		expected queryResult
+		err      string
+	}{
+		{
+			name:    "invalid options",
+			path:    "_all_docs",
+			options: map[string]interface{}{"foo": make(chan int)},
+			err:     "cannot convert type chan int to []string",
+		},
+		{
+			name: "network error",
+			path: "_all_docs",
+			db:   newTestDB(nil, errors.New("go away")),
+			err:  "Get http://example.com/testdb/_all_docs: go away",
+		},
+		{
+			name: "error response",
+			path: "_all_docs",
+			db: newTestDB(&http.Response{
+				StatusCode: kivik.StatusBadRequest,
+				Body:       ioutil.NopCloser(strings.NewReader("")),
+			}, nil),
+			err: "Bad Request",
+		},
+		{
+			name: "all docs default 1.6.1",
+			path: "_all_docs",
+			db: newTestDB(&http.Response{
+				StatusCode: kivik.StatusOK,
+				Header: map[string][]string{
+					"Transfer-Encoding": {"chunked"},
+					"Date":              {"Tue, 24 Oct 2017 21:17:12 GMT"},
+					"Server":            {"CouchDB/1.6.1 (Erlang OTP/17)"},
+					"ETag":              {`"2MVNDK3T2PN4JUK89TKD10QDA"`},
+					"Content-Type":      {"text/plain; charset=utf-8"},
+					"Cache-Control":     {"must-revalidate"},
+				},
+				Body: ioutil.NopCloser(strings.NewReader(`{"total_rows":3,"offset":0,"rows":[
+{"id":"_design/_auth","key":"_design/_auth","value":{"rev":"1-75efcce1f083316d622d389f3f9813f7"}},
+{"id":"org.couchdb.user:5wmxzru3b4i6pdmvhslq5egiye","key":"org.couchdb.user:5wmxzru3b4i6pdmvhslq5egiye","value":{"rev":"1-747e6766038164010fd0efcabd1a31dd"}},
+{"id":"org.couchdb.user:zqfdn6u3cqi6pol3hslq5egiye","key":"org.couchdb.user:zqfdn6u3cqi6pol3hslq5egiye","value":{"rev":"1-4645438e6e1aa2230a1b06b5c1f5c63f"}}
+]}
+`)),
+			}, nil),
+			expected: queryResult{
+				TotalRows: 3,
+				Rows: []driver.Row{
+					{
+						ID:    "_design/_auth",
+						Key:   []byte(`"_design/_auth"`),
+						Value: []byte(`{"rev":"1-75efcce1f083316d622d389f3f9813f7"}`),
+					},
+					{
+						ID:    "org.couchdb.user:5wmxzru3b4i6pdmvhslq5egiye",
+						Key:   []byte(`"org.couchdb.user:5wmxzru3b4i6pdmvhslq5egiye"`),
+						Value: []byte(`{"rev":"1-747e6766038164010fd0efcabd1a31dd"}`),
+					},
+					{
+						ID:    "org.couchdb.user:zqfdn6u3cqi6pol3hslq5egiye",
+						Key:   []byte(`"org.couchdb.user:zqfdn6u3cqi6pol3hslq5egiye"`),
+						Value: []byte(`{"rev":"1-4645438e6e1aa2230a1b06b5c1f5c63f"}`),
+					},
+				},
+			},
+		},
+		{
+			name: "all docs options 1.6.1",
+			path: "/_all_docs?update_seq=true&limit=1&skip=1",
+			db: newTestDB(&http.Response{
+				StatusCode: kivik.StatusOK,
+				Header: map[string][]string{
+					"Transfer-Encoding": {"chunked"},
+					"Date":              {"Tue, 24 Oct 2017 21:17:12 GMT"},
+					"Server":            {"CouchDB/1.6.1 (Erlang OTP/17)"},
+					"ETag":              {`"2MVNDK3T2PN4JUK89TKD10QDA"`},
+					"Content-Type":      {"text/plain; charset=utf-8"},
+					"Cache-Control":     {"must-revalidate"},
+				},
+				Body: ioutil.NopCloser(strings.NewReader(`{"total_rows":3,"offset":1,"update_seq":31,"rows":[
+{"id":"org.couchdb.user:5wmxzru3b4i6pdmvhslq5egiye","key":"org.couchdb.user:5wmxzru3b4i6pdmvhslq5egiye","value":{"rev":"1-747e6766038164010fd0efcabd1a31dd"}}
+]}
+`)),
+			}, nil),
+			expected: queryResult{
+				TotalRows: 3,
+				Offset:    1,
+				UpdateSeq: "31",
+				Rows: []driver.Row{
+					{
+						ID:    "org.couchdb.user:5wmxzru3b4i6pdmvhslq5egiye",
+						Key:   []byte(`"org.couchdb.user:5wmxzru3b4i6pdmvhslq5egiye"`),
+						Value: []byte(`{"rev":"1-747e6766038164010fd0efcabd1a31dd"}`),
+					},
+				},
+			},
+		},
+		{
+			name: "all docs options 2.0.0, no results",
+			path: "/_all_docs?update_seq=true&limit=1",
+			db: newTestDB(&http.Response{
+				StatusCode: kivik.StatusOK,
+				Header: map[string][]string{
+					"Transfer-Encoding":  {"chunked"},
+					"Date":               {"Tue, 24 Oct 2017 21:21:30 GMT"},
+					"Server":             {"CouchDB/2.0.0 (Erlang OTP/17)"},
+					"Content-Type":       {"application/json"},
+					"Cache-Control":      {"must-revalidate"},
+					"X-Couch-Request-ID": {"a9688d9335"},
+					"X-Couch-Body-Time":  {"0"},
+				},
+				Body: ioutil.NopCloser(strings.NewReader(`{"total_rows":1,"offset":0,"update_seq":"13-g1AAAAEzeJzLYWBg4MhgTmHgzcvPy09JdcjLz8gvLskBCjPlsQBJhgdA6j8QZCUy4FV3AKLuflYiE151DRB18wmZtwCibj9u85ISgGRSPV63JSmA1NiD1bDgUJPIkCSP3xAHkCHxYDWsWQDg12MD","rows":[
+{"id":"_design/_auth","key":"_design/_auth","value":{"rev":"1-75efcce1f083316d622d389f3f9813f7"}}
+]}
+`)),
+			}, nil),
+			expected: queryResult{
+				TotalRows: 1,
+				UpdateSeq: "13-g1AAAAEzeJzLYWBg4MhgTmHgzcvPy09JdcjLz8gvLskBCjPlsQBJhgdA6j8QZCUy4FV3AKLuflYiE151DRB18wmZtwCibj9u85ISgGRSPV63JSmA1NiD1bDgUJPIkCSP3xAHkCHxYDWsWQDg12MD",
+				Rows: []driver.Row{
+					{
+						ID:    "_design/_auth",
+						Key:   []byte(`"_design/_auth"`),
+						Value: []byte(`{"rev":"1-75efcce1f083316d622d389f3f9813f7"}`),
+					},
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			rows, err := test.db.rowsQuery(context.Background(), test.path, test.options)
+			testy.Error(t, test.err, err)
+			result := queryResult{
+				Rows: []driver.Row{},
+			}
+			for {
+				var row driver.Row
+				if e := rows.Next(&row); e != nil {
+					if e != io.EOF {
+						result.Err = e.Error()
+					}
+					break
+				}
+				result.Rows = append(result.Rows, row)
+			}
+			result.Offset = rows.Offset()
+			result.TotalRows = rows.TotalRows()
+			result.UpdateSeq = rows.UpdateSeq()
+			if warner, ok := rows.(driver.RowsWarner); ok {
+				result.Warning = warner.Warning()
+			} else {
+				t.Errorf("RowsWarner interface not satisified!!?")
+			}
+
+			if d := diff.Interface(test.expected, result); d != nil {
+				t.Error(d)
 			}
 		})
 	}
