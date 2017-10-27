ce18ed6fa05a1b9f9f960975653ca1f904ea5045
Jonathan Hall <flimzy@flimzy.com>
2017-10-27 17:12:13 +0200

    Complete tests around DBExists() method

--- client_test.go
+++ client_test.go
@@ -2,8 +2,12 @@ package couchdb
 
 import (
 	"context"
+	"errors"
+	"net/http"
 	"testing"
 
+	"github.com/flimzy/kivik"
+	"github.com/flimzy/testy"
 	"github.com/go-kivik/kiviktest/kt"
 )
 
@@ -16,20 +20,74 @@ func TestAllDBs(t *testing.T) {
 }
 
 func TestDBExists(t *testing.T) {
-	client := getClient(t)
-	exists, err := client.DBExists(context.Background(), "_users", nil)
-	if err != nil {
-		t.Fatalf("Failed: %s", err)
+	tests := []struct {
+		name   string
+		client *client
+		dbName string
+		exists bool
+		status int
+		err    string
+	}{
+		{
+			name:   "no db specified",
+			status: kivik.StatusBadRequest,
+			err:    "kivik: dbName required",
+		},
+		{
+			name:   "network error",
+			dbName: "foo",
+			client: newTestClient(nil, errors.New("net error")),
+			status: 500,
+			err:    "Head http://example.com/foo: net error",
+		},
+		{
+			name:   "not found, 1.6.1",
+			dbName: "foox",
+			client: newTestClient(&http.Response{
+				StatusCode: 404,
+				Header: http.Header{
+					"Server":         {"CouchDB/1.6.1 (Erlang OTP/17)"},
+					"Date":           {"Fri, 27 Oct 2017 15:09:19 GMT"},
+					"Content-Type":   {"text/plain; charset=utf-8"},
+					"Content-Length": {"44"},
+					"Cache-Control":  {"must-revalidate"},
+				},
+				Body: Body(""),
+			}, nil),
+			exists: false,
+		},
+		{
+			name:   "exists, 1.6.1",
+			dbName: "foo",
+			client: newTestClient(&http.Response{
+				StatusCode: 200,
+				Header: http.Header{
+					"Server":         {"CouchDB/1.6.1 (Erlang OTP/17)"},
+					"Date":           {"Fri, 27 Oct 2017 15:09:19 GMT"},
+					"Content-Type":   {"text/plain; charset=utf-8"},
+					"Content-Length": {"229"},
+					"Cache-Control":  {"must-revalidate"},
+				},
+				Body: Body(""),
+			}, nil),
+			exists: true,
+		},
 	}
-	if !exists {
-		t.Error("Expected _users to exist")
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			exists, err := test.client.DBExists(context.Background(), test.dbName, nil)
+			testy.StatusError(t, test.err, test.status, err)
+			if exists != test.exists {
+				t.Errorf("Unexpected result: %t", exists)
+			}
+		})
 	}
 }
 
 func TestCreateAndDestroyDB(t *testing.T) {
 	client := getClient(t)
 	dbName := kt.TestDBName(t)
-	defer client.DestroyDB(context.Background(), dbName, nil)
+	defer client.DestroyDB(context.Background(), dbName, nil) // nolint: errcheck
 	if err := client.CreateDB(context.Background(), dbName, nil); err != nil {
 		t.Errorf("Create failed: %s", err)
 	}
