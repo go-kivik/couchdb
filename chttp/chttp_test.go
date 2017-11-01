b04d310b7d76827246f8a6958ba1f8825fe47360
Jonathan Hall <flimzy@flimzy.com>
2017-11-01 20:44:31 +0100

    redundant

--- chttp/chttp_test.go
+++ chttp/chttp_test.go
@@ -3,12 +3,10 @@ package chttp
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"mime"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
@@ -132,48 +130,6 @@ func getClient(t *testing.T) *Client {
 		t.Fatalf("Failed to connect to '%s': %s", dsn, err)
 	}
 	return client
-}
-
-func TestDo(t *testing.T) {
-	client := getClient(t)
-	res, err := client.DoReq(context.Background(), "GET", "/", &Options{Accept: "application/json"})
-	if err != nil {
-		t.Errorf("Failed to make request GET /: %s", err)
-	}
-	body := &bytes.Buffer{}
-	if _, err = body.ReadFrom(res.Body); err != nil {
-		t.Errorf("Failed to read response body: %s", err)
-	}
-	if !strings.Contains(body.String(), `"couchdb"`) {
-		t.Errorf("Body does not contain `\"couchdb\"` as expected: `%s`", body)
-	}
-	var i interface{}
-	if err = json.Unmarshal(body.Bytes(), &i); err != nil {
-		t.Errorf("Body is not valid JSON: %s", err)
-	}
-	ct, _, err := mime.ParseMediaType(res.Header.Get("Content-Type"))
-	if err != nil {
-		t.Errorf("Failed to parse content type: %s", err)
-	}
-	if ct != "application/json" {
-		t.Errorf("Unexpected content type: %s", ct)
-	}
-}
-
-func TestJSONBody(t *testing.T) {
-	client := getClient(t)
-	bogusQuery := map[string]string{
-		"foo": "bar",
-		"bar": "baz",
-	}
-	buf := &bytes.Buffer{}
-	if err := json.NewEncoder(buf).Encode(bogusQuery); err != nil {
-		t.Fatalf("JSON encoding failed: %s", err)
-	}
-	_, err := client.DoReq(context.Background(), "POST", "/_session", &Options{Body: buf})
-	if err != nil {
-		t.Errorf("Failed to make request: %s", err)
-	}
 }
 
 func TestFixPath(t *testing.T) {
