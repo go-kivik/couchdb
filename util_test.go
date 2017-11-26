040b3523565fe2cd2279cc1b2004a47a60ddcf4d
Jonathan Hall <flimzy@flimzy.com>
2017-11-26 19:02:06 +0100

    Remove unused utility function

--- util_test.go
+++ util_test.go
@@ -1,9 +1,7 @@
 package couchdb
 
 import (
-	"bytes"
 	"encoding/json"
-	"strings"
 	"testing"
 
 	"github.com/flimzy/diff"
@@ -52,59 +50,6 @@ func TestDeJSONify(t *testing.T) {
 			testy.StatusError(t, test.err, test.status, err)
 			if d := diff.Interface(test.expected, result); d != nil {
 				t.Error(d)
-			}
-		})
-	}
-}
-
-func TestToJSON(t *testing.T) {
-	tests := []struct {
-		Name     string
-		Input    interface{}
-		Expected string
-		status   int
-		err      string
-	}{
-		{
-			Name:     "Null",
-			Expected: "null",
-		},
-		{
-			Name:     "String",
-			Input:    `{"foo":"bar"}`,
-			Expected: `{"foo":"bar"}`,
-		},
-		{
-			Name:     "ByteSlice",
-			Input:    []byte(`{"foo":"bar"}`),
-			Expected: `{"foo":"bar"}`,
-		},
-		{
-			Name:     "RawMessage",
-			Input:    json.RawMessage(`{"foo":"bar"}`),
-			Expected: `{"foo":"bar"}`,
-		},
-		{
-			Name:     "Interface",
-			Input:    map[string]string{"foo": "bar"},
-			Expected: `{"foo":"bar"}`,
-		},
-		{
-			Name:   "invalid json",
-			Input:  make(chan int),
-			status: kivik.StatusBadRequest,
-			err:    "json: unsupported type: chan int",
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.Name, func(t *testing.T) {
-			r, err := toJSON(test.Input)
-			testy.StatusError(t, test.err, test.status, err)
-			buf := &bytes.Buffer{}
-			_, _ = buf.ReadFrom(r)
-			result := strings.TrimSpace(buf.String())
-			if result != test.Expected {
-				t.Errorf("Expected: `%s`\n  Actual: `%s`", test.Expected, result)
 			}
 		})
 	}
