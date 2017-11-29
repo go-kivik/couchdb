4553c0510dff0ecc0fcedb88d87a7d5a1ef00dfb
Jonathan Hall <flimzy@flimzy.com>
2017-11-29 19:22:44 +0100

    Doc improvements

--- constants.go
+++ constants.go
@@ -12,10 +12,11 @@ const (
 	//    db.Put(ctx, "doc_id", doc, kivik.Options{couchdb.OptionFullCommit: true})
 	OptionFullCommit = "X-Couch-Full-Commit"
 
-	// OptionIfNoneMatch sets the If-None-Match header on the request.
+	// OptionIfNoneMatch is an option key to set the If-None-Match header on
+	// the request.
 	//
 	// Example:
 	//
-	//    row, err := db.Get(ctx, "doc_id", kivik.Options(couchdb.OptionIfNoneMatch: "1-xxx"))
+	//    row, err := db.Get(ctx, "doc_id", kivik.Options{couchdb.OptionIfNoneMatch: "1-xxx"})
 	OptionIfNoneMatch = "If-None-Match"
 )
