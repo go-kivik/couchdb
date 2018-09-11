a96ad63241e2f0cd677e634dce2b7b8ea049f789
Jonathan Hall <flimzy@flimzy.com>
2018-09-11 21:13:44 +0200

    Prevent multiple simultaneous auth requests

--- chttp/cookieauth.go
+++ chttp/cookieauth.go
@@ -53,21 +53,35 @@ var authInProgress = &struct{ name string }{"in progress"}
 // RoundTrip fulfills the http.RoundTripper interface. It sets
 // (re-)authenticates when the cookie has expired or is not yet set.
 func (a *CookieAuth) RoundTrip(req *http.Request) (*http.Response, error) {
-	ctx := req.Context()
-	if inProg, _ := ctx.Value(authInProgress).(bool); !inProg {
-		// this means we aren't in the process of authenticating already
-		if _, ok := req.Header["Cookie"]; !ok {
-			// No cookie set, so attempt auth
-			ctx = context.WithValue(ctx, authInProgress, true)
-			opts := &Options{
-				Body: EncodeBody(a),
-			}
-			if _, err := a.client.DoError(ctx, kivik.MethodPost, "/_session", opts); err != nil {
-				return nil, err
-			}
-			// Now make sure the original request is authenticated
-			req.AddCookie(a.Cookie())
-		}
+	if err := a.authenticate(req); err != nil {
+		return nil, err
 	}
 	return a.transport.RoundTrip(req)
+}
+
+func (a *CookieAuth) authenticate(req *http.Request) error {
+	ctx := req.Context()
+	if inProg, _ := ctx.Value(authInProgress).(bool); inProg {
+		return nil
+	}
+	if _, ok := req.Header["Cookie"]; ok {
+		// Request already authenticated
+		return nil
+	}
+	a.client.authMU.Lock()
+	defer a.client.authMU.Unlock()
+	if c := a.Cookie(); c != nil {
+		// In case another simultaneous process authenticated successfully first
+		req.AddCookie(c)
+		return nil
+	}
+	ctx = context.WithValue(ctx, authInProgress, true)
+	opts := &Options{
+		Body: EncodeBody(a),
+	}
+	if _, err := a.client.DoError(ctx, kivik.MethodPost, "/_session", opts); err != nil {
+		return err
+	}
+	req.AddCookie(a.Cookie())
+	return nil
 }
