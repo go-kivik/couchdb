f8aef142ab12bb1c46121f10ca8e472df9c79012
Jonathan Hall <flimzy@flimzy.com>
2019-01-24 19:45:39 +0100

    Add SetCookie authenticator, and full auth tests

--- auth_test.go
+++ auth_test.go
@@ -3,6 +3,8 @@ package couchdb
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/flimzy/testy"
@@ -77,4 +79,124 @@ func TestAuthenticate(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestAuthentication(t *testing.T) {
+	type tst struct {
+		handler    func(*testing.T) http.Handler
+		setup      func(*testing.T, *client)
+		auther     Authenticator
+		authStatus int
+		authErr    string
+		status     int
+		err        string
+	}
+
+	handler200 := func(_ *testing.T) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+			w.WriteHeader(200)
+		})
+	}
+	tests := testy.NewTable()
+	tests.Add("SetTransport", tst{
+		handler: handler200,
+		auther: SetTransport(customTransport(func(r *http.Request) (*http.Response, error) {
+			return nil, errors.New("transport error")
+		})),
+		status: kivik.StatusNetworkError,
+		err:    "transport error",
+	})
+	tests.Add("SetTransport again", tst{
+		handler: handler200,
+		auther: SetTransport(customTransport(func(r *http.Request) (*http.Response, error) {
+			return nil, errors.New("transport error")
+		})),
+		setup: func(t *testing.T, c *client) {
+			c.Client.Client.Transport = http.DefaultTransport
+		},
+		authStatus: kivik.StatusBadAPICall,
+		authErr:    "kivik: HTTP client transport already set",
+	})
+	tests.Add("BasicAuth", tst{
+		handler: func(t *testing.T) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if h := r.Header.Get("Authorization"); h != "Basic Ym9iOmFiYzEyMw==" {
+					t.Errorf("Unexpected Auth header: %s\n", h)
+				}
+				w.WriteHeader(200)
+				_, _ = w.Write([]byte(`{}`))
+			})
+		},
+		auther: BasicAuth("bob", "abc123"),
+	})
+	tests.Add("CookieAuth", tst{
+		handler: func(t *testing.T) http.Handler {
+			expectedPaths := []string{"/_session", "/"}
+			i := -1
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				i++
+				if p := r.URL.Path; p != expectedPaths[i] {
+					t.Errorf("Unexpected path: %s\n", p)
+				}
+				w.WriteHeader(200)
+				_, _ = w.Write([]byte(`{}`))
+			})
+		},
+		auther: CookieAuth("bob", "abc123"),
+	})
+	tests.Add("SetCookie", tst{
+		handler: func(t *testing.T) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				c, err := r.Cookie("cow")
+				if err != nil {
+					t.Fatal(err)
+				}
+				if c.Value != "moo" {
+					t.Errorf("Unexpected cookie value: %s\n", c.Value)
+				}
+				w.WriteHeader(200)
+				_, _ = w.Write([]byte(`{}`))
+			})
+		},
+		auther: SetCookie(&http.Cookie{Name: "cow", Value: "moo"}),
+	})
+	tests.Add("SetCookie again", tst{
+		handler: func(t *testing.T) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				c, err := r.Cookie("cow")
+				if err != nil {
+					t.Fatal(err)
+				}
+				if c.Value != "moo" {
+					t.Errorf("Unexpected cookie value: %s\n", c.Value)
+				}
+				w.WriteHeader(200)
+				_, _ = w.Write([]byte(`{}`))
+			})
+		},
+		auther: SetCookie(&http.Cookie{Name: "cow", Value: "moo"}),
+		setup: func(t *testing.T, c *client) {
+			c.Client.Client.Transport = http.DefaultTransport
+		},
+		authStatus: kivik.StatusBadAPICall,
+		authErr:    "kivik: HTTP client transport already set",
+	})
+
+	driver := &Couch{}
+	tests.Run(t, func(t *testing.T, test tst) {
+		s := httptest.NewServer(test.handler(t))
+		defer s.Close()
+		driverClient, err := driver.NewClient(s.URL)
+		if err != nil {
+			t.Fatal(err)
+		}
+		client := driverClient.(*client)
+		if test.setup != nil {
+			test.setup(t, client)
+		}
+		err = client.Authenticate(context.Background(), test.auther)
+		testy.StatusError(t, test.authErr, test.authStatus, err)
+		_, err = client.Version(context.Background())
+		testy.StatusErrorRE(t, test.err, test.status, err)
+	})
 }
