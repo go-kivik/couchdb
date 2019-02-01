ca868623ffe5fc63fd1c29113f38c10e664027f3
Jonathan Hall <flimzy@flimzy.com>
2019-02-01 13:28:14 +0100

    Add ClusterSetup() support

--- cluster.go
+++ cluster.go
@@ -7,8 +7,6 @@ import (
 	"github.com/go-kivik/couchdb/chttp"
 )
 
-const optionEnsureDBsExist = "ensure_dbs_exist"
-
 func (c *client) ClusterStatus(ctx context.Context, opts map[string]interface{}) (string, error) {
 	var result struct {
 		State string `json:"state"`
@@ -19,4 +17,12 @@ func (c *client) ClusterStatus(ctx context.Context, opts map[string]interface{})
 	}
 	_, err = c.DoJSON(ctx, http.MethodGet, "/_cluster_setup", &chttp.Options{Query: query}, &result)
 	return result.State, err
+}
+
+func (c *client) ClusterSetup(ctx context.Context, action interface{}) error {
+	options := &chttp.Options{
+		Body: chttp.EncodeBody(action),
+	}
+	_, err := c.DoError(ctx, http.MethodPost, "/_cluster_setup", options)
+	return err
 }
--- cluster_test.go
+++ cluster_test.go
@@ -15,6 +15,8 @@ import (
 	"github.com/go-kivik/kivik/errors"
 )
 
+const optionEnsureDBsExist = "ensure_dbs_exist"
+
 func TestClusterStatus(t *testing.T) {
 	type tst struct {
 		client   *client
@@ -99,5 +101,76 @@ func TestClusterStatus(t *testing.T) {
 		if result != test.expected {
 			t.Errorf("Unexpected result:\nExpected: %s\n  Actual: %s\n", test.expected, result)
 		}
+	})
+}
+
+func TestClusterSetup(t *testing.T) {
+	type tst struct {
+		client *client
+		action interface{}
+		status int
+		err    string
+	}
+	tests := testy.NewTable()
+	tests.Add("network error", tst{
+		client: newTestClient(nil, errors.New("network error")),
+		status: kivik.StatusNetworkError,
+		err:    "Post http://example.com/_cluster_setup: network error",
+	})
+	tests.Add("invalid action", tst{
+		client: newTestClient(nil, nil),
+		action: func() {},
+		status: kivik.StatusBadAPICall,
+		err:    "Post http://example.com/_cluster_setup: json: unsupported type: func()",
+	})
+	tests.Add("success", func(t *testing.T) interface{} {
+		return tst{
+			client: newCustomClient(func(r *http.Request) (*http.Response, error) {
+				expected := map[string]interface{}{
+					"action": "finish_cluster",
+				}
+				result := map[string]interface{}{}
+				if err := json.NewDecoder(r.Body).Decode(&result); err != nil {
+					t.Fatal(err)
+				}
+				if d := diff.Interface(expected, result); d != nil {
+					t.Errorf("Unexpected request body:\n%s\n", d)
+				}
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					ProtoMajor: 1,
+					ProtoMinor: 1,
+					Header: http.Header{
+						"Content-Type": []string{"application/json"},
+					},
+					Body: ioutil.NopCloser(strings.NewReader(`{"ok":true}`)),
+				}, nil
+			}),
+			action: map[string]interface{}{
+				"action": "finish_cluster",
+			},
+		}
+	})
+	tests.Add("already finished", tst{
+		client: newTestClient(&http.Response{
+			StatusCode:    http.StatusBadRequest,
+			ProtoMajor:    1,
+			ProtoMinor:    1,
+			ContentLength: 63,
+			Header: http.Header{
+				"Content-Type": []string{"application/json"},
+			},
+			Body: ioutil.NopCloser(strings.NewReader(`{"error":"bad_request","reason":"Cluster is already finished"}`)),
+		}, nil),
+		action: map[string]interface{}{
+			"action": "finish_cluster",
+		},
+		status: kivik.StatusBadRequest,
+		err:    "Bad Request: Cluster is already finished",
+	})
+
+	tests.Run(t, func(t *testing.T, test tst) {
+		err := test.client.ClusterSetup(context.Background(), test.action)
+		testy.StatusError(t, test.err, test.status, err)
 	})
 }
