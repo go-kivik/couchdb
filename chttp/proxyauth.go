25f6d53fdaf538d2493114ea535ecdb880e8acc4
Jonathan Hall <flimzy@flimzy.com>
2019-06-10 14:56:21 +0200

    Cache HMAC calculation for greater efficiency

--- chttp/proxyauth.go
+++ chttp/proxyauth.go
@@ -15,6 +15,7 @@ type ProxyAuth struct {
 	Headers  http.Header
 
 	transport http.RoundTripper
+	token     string
 }
 
 var _ Authenticator = &ProxyAuth{}
@@ -26,21 +27,29 @@ func (a *ProxyAuth) header(header string) string {
 	return header
 }
 
+func (a *ProxyAuth) genToken() string {
+	if a.Secret == "" {
+		return ""
+	}
+	if a.token != "" {
+		return a.token
+	}
+	// Generate auth token
+	// https://docs.couchdb.org/en/stable/config/auth.html#couch_httpd_auth/x_auth_token
+	h := hmac.New(sha1.New, []byte(a.Secret))
+	_, _ = h.Write([]byte(a.Username))
+	a.token = hex.EncodeToString(h.Sum(nil))
+	return a.token
+}
+
 func (a *ProxyAuth) RoundTrip(req *http.Request) (*http.Response, error) {
 	// Convert roles slice to comma separated values
 	rolesCsv := strings.Join(a.Roles[:], ",")
 
-	// If the secret is an empty string, do not calculate the token
-	if a.Secret != "" {
-		// Generate auth token
-		// https://docs.couchdb.org/en/stable/config/auth.html#couch_httpd_auth/x_auth_token
-		h := hmac.New(sha1.New, []byte(a.Secret))
-		_, _ = h.Write([]byte(a.Username))
-		token := hex.EncodeToString(h.Sum(nil))
+	if token := a.genToken(); token != "" {
 		req.Header.Set(a.header("X-Auth-CouchDB-Token"), token)
 	}
 
-	// Add headers to request
 	req.Header.Set(a.header("X-Auth-CouchDB-UserName"), a.Username)
 	req.Header.Set(a.header("X-Auth-CouchDB-Roles"), rolesCsv)
 
