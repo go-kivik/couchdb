ba0d89f083bcc539c8263c651edadd7d5fb3f02d
Jonathan Hall <flimzy@flimzy.com>
2020-05-02 12:32:25 +0200

    Run multi-query test against CouchDB 3.x too

--- db_live_test.go
+++ db_live_test.go
@@ -99,9 +99,45 @@ func TestQueries_2_x(t *testing.T) {
 	}
 }
 
-// func TestQueries_3_x(t *testing.T) {
-// 	dsn := os.Getenv("KIVIK_TEST_DSN_COUCH30")
-// 	if dsn == "" {
-// 		t.Skip("KIVIK_TEST_DSN_COUCH30 not configured")
-// 	}
-// }
+func TestQueries_3_x(t *testing.T) {
+	dsn := os.Getenv("KIVIK_TEST_DSN_COUCH30")
+	if dsn == "" {
+		t.Skip("KIVIK_TEST_DSN_COUCH30 not configured")
+	}
+
+	client, err := kivik.New("couch", dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	db := client.DB(context.Background(), "_users")
+	rows, err := db.AllDocs(context.Background(), map[string]interface{}{
+		"queries": []map[string]interface{}{
+			{},
+			{},
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rows.Close() // nolint:errcheck
+	result := make([]interface{}, 0)
+	for rows.Next() {
+		if rows.EOQ() {
+			result = append(result, map[string]interface{}{
+				"EOQ":        true,
+				"total_rows": rows.TotalRows(),
+			})
+			continue
+		}
+		result = append(result, map[string]interface{}{
+			"_id": rows.ID(),
+		})
+	}
+	if err := rows.Err(); err != nil {
+		t.Fatal(err)
+	}
+	if d := testy.DiffInterface(testy.Snapshot(t), result); d != nil {
+		t.Error(d)
+	}
+}
