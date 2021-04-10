c7f399f721bb8e2b82620771de37e0cd042f846a
Jonathan Hall <flimzy@flimzy.com>
2021-04-10 18:01:23 +0200

    Bump version constant for next release

--- constants.go
+++ constants.go
@@ -1,7 +1,7 @@
 package couchdb
 
 // Version is the current version of this package.
-const Version = "3.2.5"
+const Version = "3.2.7"
 
 const (
 	// OptionFullCommit is the option key used to set the `X-Couch-Full-Commit`
