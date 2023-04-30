ef258fb7c857683304bf8deb180206c6a9e78ece
Jonathan Hall <flimzy@flimzy.com>
2023-04-30 22:30:12 +0200

    Delete tests for 1.x series which are no longer executed

--- db_live_test.go
+++ db_live_test.go
@@ -22,50 +22,6 @@ import (
 	"github.com/go-kivik/kivik/v4"
 )
 
-func TestQueries_1_x(t *testing.T) {
-	dsn := os.Getenv("KIVIK_TEST_DSN_COUCH17")
-	if dsn == "" {
-		t.Skip("KIVIK_TEST_DSN_COUCH17 not configured")
-	}
-
-	client, err := kivik.New("couch", dsn)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	db := client.DB("_users")
-	rows := db.AllDocs(context.Background(), map[string]interface{}{
-		"queries": []map[string]interface{}{
-			{},
-			{},
-		},
-	})
-	if err := rows.Err(); err != nil {
-		t.Fatal(err)
-	}
-	defer rows.Close() // nolint:errcheck
-	result := make([]interface{}, 0)
-	for rows.Next() {
-		result = append(result, map[string]interface{}{
-			"_id": rows.ID(),
-		})
-	}
-	meta, err := rows.Metadata()
-	if err != nil {
-		t.Fatal(err)
-	}
-	wantMeta := &kivik.ResultMetadata{}
-	if d := testy.DiffInterface(wantMeta, meta); d != nil {
-		t.Error(d)
-	}
-	if err := rows.Err(); err != nil {
-		t.Fatal(err)
-	}
-	if d := testy.DiffInterface(testy.Snapshot(t), result); d != nil {
-		t.Error(d)
-	}
-}
-
 func TestQueries_2_x(t *testing.T) {
 	dsn := os.Getenv("KIVIK_TEST_DSN_COUCH23")
 	if dsn == "" {
