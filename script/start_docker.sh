af0f9e5af33a22c9047af852337abe2ddea62362
Jonathan Hall <flimzy@flimzy.com>
2023-05-28 22:05:16 +0200

    Add CouchDB 3.2 suite

--- script/start_docker.sh
+++ script/start_docker.sh
@@ -11,6 +11,7 @@ export KIVIK_TEST_DSN_COUCH22=http://admin:abc123@localhost:6002/
 export KIVIK_TEST_DSN_COUCH23=http://admin:abc123@localhost:6003/
 export KIVIK_TEST_DSN_COUCH30=http://admin:abc123@localhost:6004/
 export KIVIK_TEST_DSN_COUCH31=http://admin:abc123@localhost:6005/
+export KIVIK_TEST_DSN_COUCH32=http://admin:abc123@localhost:6006/
 
 echo "CouchDB 2.2.0"
 docker pull couchdb:2.2.0
@@ -31,3 +32,8 @@ echo "CouchDB 3.1.2"
 docker pull apache/couchdb:3.1.2
 docker run --name couch31 -p 6005:5984/tcp -d --rm -e COUCHDB_USER -e COUCHDB_PASSWORD apache/couchdb:3.1.2
 ${DIR}/complete_couch2.sh $KIVIK_TEST_DSN_COUCH31
+
+echo "CouchDB 3.2.3"
+docker pull apache/couchdb:3.2.3
+docker run --name couch32 -p 6006:5984/tcp -d --rm -e COUCHDB_USER -e COUCHDB_PASSWORD apache/couchdb:3.2.3
+${DIR}/complete_couch2.sh $KIVIK_TEST_DSN_COUCH32
